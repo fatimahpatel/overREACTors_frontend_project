51bc0a9a05329a1f2c935246a03b67506da41e99
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-31 12:46:01 +0000

    Removed previous search bar and drop down, created new one to filter book by title

--- biblion/src/containers/BookContainer.js
+++ biblion/src/containers/BookContainer.js
@@ -6,13 +6,39 @@ const SERVER_URL = "http://localhost:8080"
 
 const BookContainer = () => {
 
+    const [searchParam] = useState(["author", "genre", "title"]);
+    const [filteredBooks, setFilteredBooks] = useState([]);
+    const [filterParam, setFilterParam] = useState(["All"]);
+    const [query, setQuery] = useState("");
+
     //GET all books 
     const [books, setBooks] = useState([]);
     useEffect(() => {
         fetch(`${SERVER_URL}/books`)
         .then(response => response.json())
-        .then(response => setBooks(response))
+        .then(response => {
+            setBooks(response)
+            setFilteredBooks(response)
+        })
     }, []);
+
+    useEffect(() => {
+
+        const foundBooks = books.filter((book) => {
+            return book.title.toLowerCase().indexOf(query.toLowerCase());
+        })
+        setFilteredBooks(foundBooks);
+
+        // searchParam.some((newBook) => {
+        //                 return (
+        //                     book[newBook]
+        //                         .toString()
+        //                         .toLowerCase()
+        //                         .indexOf(query.toLowerCase()) > -1
+        //                 );
+        //             });
+
+    }, [query]) //query is the state that changes
     
     //Filter books
     const filterBooks = (searchTerm) => {
@@ -23,33 +49,40 @@ const BookContainer = () => {
       }
 
     //Search bar and dropdown 
-    const [searchParam] = useState(["author", "genre", "title"]);
-    const [filteredBooks, setFilteredBooks] = useState();
-    const [filterParam, setFilterParam] = useState(["All"]);
-    const [q, setQ] = useState("");
+  
 
-    function search(books) {
-        return books.filter((book) => {
-            if (book.genre == filterParam) {
-                return searchParam.some((newBook) => {
-                    return (
-                        book[newBook]
-                            .toString()
-                            .toLowerCase()
-                            .indexOf(q.toLowerCase()) > -1
-                    );
-                });
-            } else if (filterParam == "All") {
-                return searchParam.some((newBook) => {
-                    return (
-                        book[newBook]
-                            .toString()
-                            .toLowerCase()
-                            .indexOf(q.toLowerCase()) > -1
-                    );
-                });
-            }
-        });
+    function handleChange(event) {
+        setQuery(event.target.value);
+
+
+
+
+
+
+
+
+
+        // return books.filter((book) => {
+        //     if (book.genre == filterParam) {
+        //         return searchParam.some((newBook) => {
+        //             return (
+        //                 book[newBook]
+        //                     .toString()
+        //                     .toLowerCase()
+        //                     .indexOf(query.toLowerCase()) > -1
+        //             );
+        //         });
+        //     } else if (filterParam == "All") {
+        //         return searchParam.some((newBook) => {
+        //             return (
+        //                 book[newBook]
+        //                     .toString()
+        //                     .toLowerCase()
+        //                     .indexOf(query.toLowerCase()) > -1
+        //             );
+        //         });
+        //     }
+        // });
     }
 
 
@@ -57,7 +90,7 @@ const BookContainer = () => {
         <>
         <h1>Biblion</h1>
 
-        <div className="dropdown">
+        {/* <div className="dropdown">
         <select onChange={(e) => {
                 setFilterParam(e.target.value);
                     }}
@@ -66,7 +99,7 @@ const BookContainer = () => {
                     <option value="All">Filter By Genre</option>
                     <option value="Romance">Romance</option>
         </select>
-        </div>
+        </div> */}
 
         {/*Search bar*/}
         <div className="wrapper">
@@ -78,19 +111,17 @@ const BookContainer = () => {
                                 id="search-form"
                                 className="search-input"
                                 placeholder="Search for..."
-                                value={q}
+                                value={query}
                                 /*
                                 // set the value of our useState q
                                 //  anytime the user types in the search box
                                 */
-                                onChange={(e) => setQ(e.target.value)}
+                                onChange={handleChange}
                             />
                         </label>
                     </div></div>
 
-        <Search filterBooks={filterBooks} />
-
-        <BookList books={books}/>
+        <BookList books={filteredBooks}/>
 
         </>
     );
