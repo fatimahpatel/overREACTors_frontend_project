c237a7137dc55642142093623865bf36611e7a8c
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-31 17:31:19 +0000

    Displayed reviews for each book

--- biblion/src/components/Book.js
+++ biblion/src/components/Book.js
@@ -1,11 +1,17 @@
 
 const Book = ({book}) => {
+    const reviews = book.reviews.map((review) => {
+        return <li><em>{review.descriptiveReview}</em> - {review.reader.name}</li>
+    })
     return ( 
         <div className="book">
             <p>Title: {book.title}</p>
             <p>Author: {book.author.name}</p>
             <p>Genre: {book.genre}</p>
             <p>Age rating: {book.ageRating}</p>
+            <ul>
+                {reviews}
+            </ul>
             <hr/>
 
 
