e5a26b786797b6ee83c135ed448da601a6ef41fd
Rebecca <rebecca.solomon446@gmail.com>
2023-01-31 10:24:41 +0000

    displays author of each book on browser

--- biblion/src/components/Book.js
+++ biblion/src/components/Book.js
@@ -3,7 +3,7 @@ const Book = ({book}) => {
     return ( 
         <div className="book">
             <p>Title: {book.title}</p>
-            {/* <p>Author: {book.author}</p> */}
+            <p>Author: {book.author.name}</p>
             <p>Genre: {book.genre}</p>
             <p>Age rating: {book.ageRating}</p>
             <hr/>
