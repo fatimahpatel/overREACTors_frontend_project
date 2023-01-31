b7106f4525bf46ea129bc84d9d61704be12057b1
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-31 12:50:56 +0000

    Removed commented out code

--- biblion/src/containers/BookContainer.js
+++ biblion/src/containers/BookContainer.js
@@ -22,74 +22,32 @@ const BookContainer = () => {
         })
     }, []);
 
-    useEffect(() => {
-
-        const foundBooks = books.filter((book) => {
-            return book.title.toLowerCase().indexOf(query.toLowerCase());
-        })
-        setFilteredBooks(foundBooks);
-
-        // searchParam.some((newBook) => {
-        //                 return (
-        //                     book[newBook]
-        //                         .toString()
-        //                         .toLowerCase()
-        //                         .indexOf(query.toLowerCase()) > -1
-        //                 );
-        //             });
-
-    }, [query]) //query is the state that changes
     
     //Filter books
-    const filterBooks = (searchTerm) => {
-        const foundBooks = books.filter(book => {
-          return book.genre.toLowerCase().includes(searchTerm.toLowerCase())
+    useEffect(() => {
+        const foundBooks = books.filter((book) => {
+            return book.title.toLowerCase().indexOf(query.toLowerCase()) > -1;
         })
         setFilteredBooks(foundBooks);
-      }
+    }, [query]) //query is the state that changes
 
-    //Search bar and dropdown 
-  
 
     function handleChange(event) {
         setQuery(event.target.value);
-
-
-
-
-
-
-
-
-
-        // return books.filter((book) => {
-        //     if (book.genre == filterParam) {
-        //         return searchParam.some((newBook) => {
-        //             return (
-        //                 book[newBook]
-        //                     .toString()
-        //                     .toLowerCase()
-        //                     .indexOf(query.toLowerCase()) > -1
-        //             );
-        //         });
-        //     } else if (filterParam == "All") {
-        //         return searchParam.some((newBook) => {
-        //             return (
-        //                 book[newBook]
-        //                     .toString()
-        //                     .toLowerCase()
-        //                     .indexOf(query.toLowerCase()) > -1
-        //             );
-        //         });
-        //     }
-        // });
     }
+
+
+    // const filterBooks = (searchTerm) => {
+    //     const foundBooks = books.filter(book => {
+    //       return book.genre.toLowerCase().includes(searchTerm.toLowerCase())
+    //     })
+    //     setFilteredBooks(foundBooks);
+    //   }
 
 
     return (  
         <>
         <h1>Biblion</h1>
-
         {/* <div className="dropdown">
         <select onChange={(e) => {
                 setFilterParam(e.target.value);
@@ -112,10 +70,6 @@ const BookContainer = () => {
                                 className="search-input"
                                 placeholder="Search for..."
                                 value={query}
-                                /*
-                                // set the value of our useState q
-                                //  anytime the user types in the search box
-                                */
                                 onChange={handleChange}
                             />
                         </label>
